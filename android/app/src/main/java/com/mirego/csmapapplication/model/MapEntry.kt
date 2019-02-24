d562e920ba01429d4ba64b9367e7c70e433ba091
Erin <erin.benderoff@gmail.com>
2019-02-24 16:42:36 -0500

    removed private

--- android/app/src/main/java/com/mirego/csmapapplication/model/MapEntry.kt
+++ android/app/src/main/java/com/mirego/csmapapplication/model/MapEntry.kt
@@ -1,25 +1,11 @@
 package com.mirego.csmapapplication.model
 
 class MapEntry {
-    private val name: String? = null
-
-    private val component: String? = null
-
-    private val notes: String? = null
-
-    private val type: String? = null
-
-    private val lat: Double? = null
-
-    private val long: Double? = null
-
-    private val address: String? = null
-
-    override fun toString(): String {
-        if (name != null) {
-            return name
-        } else {
-            return "HELLO"
-        }
-    }
+    val name: String? = null
+    val component: String? = null
+    val notes: String? = null
+    val type: String? = null
+    val lat: Double? = null
+    val long: Double? = null
+    val address: String? = null
 }
