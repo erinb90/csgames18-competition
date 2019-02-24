7af12c31a0da58d5dc3e2d61f324a820f4e8702d
Jacob Gagne <gagne.jacob@gmail.com>
2019-02-24 15:32:20 -0500

    added gitignore and updated dependencies

--- android/app/src/main/java/com/mirego/csmapapplication/fragment/MapSegmentFragment.kt
+++ android/app/src/main/java/com/mirego/csmapapplication/fragment/MapSegmentFragment.kt
@@ -1,27 +1,23 @@
 package com.mirego.csmapapplication.fragment
 
-import android.support.v4.app.Fragment
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
+import android.support.annotation.DrawableRes
+import android.support.v4.app.Fragment
+import android.support.v4.content.res.ResourcesCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.BitmapDescriptor
-import com.mirego.csmapapplication.R
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.fragment_map.mapView
-import kotlinx.android.synthetic.main.fragment_map.view.mapView
-import android.opengl.ETC1.getHeight
-import android.opengl.ETC1.getWidth
-import android.graphics.Canvas
-import android.graphics.Bitmap
-import android.support.v4.content.res.ResourcesCompat
-import android.graphics.drawable.Drawable
-import android.support.annotation.DrawableRes
-
+import com.mirego.csmapapplication.R
+import kotlinx.android.synthetic.main.fragment_map.*
+import kotlinx.android.synthetic.main.fragment_map.view.*
 
 
 class MapSegmentFragment : Fragment(), OnMapReadyCallback {
@@ -34,11 +30,11 @@ class MapSegmentFragment : Fragment(), OnMapReadyCallback {
         return inflater.inflate(R.layout.fragment_map, container, false).also { mapSegmentView ->
             mapSegmentView.mapView.onCreate(savedInstanceState)
             mapSegmentView.mapView.getMapAsync { map ->
-                map.addMarker(
-                    MarkerOptions()
-                        .position(LatLng(46.7794201,-71.2778703))
-                        .title("Test Opin")
-                        .icon(createPinForPart(R.drawable.ic_part_bulb))
+                    map.addMarker(
+                        MarkerOptions()
+                            .position(LatLng(46.7794201,-71.2778703))
+                            .title("Test Opin")
+                            .icon(createPinForPart(R.drawable.ic_part_bulb))
                 )
             }
         }
